3842f703de58ed74e7ceacf8d3b7188b1dcbcc19
Matias <krepchukmatias@outlook.com>
2023-10-26 22:51:59 -0300

    feature responsive

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import css from './img/css.png';
 import html from './img/html.png';
 import sass from './img/sass.png';
 import js from './img/js.png';
-
+import net from './img/net.png';
+import git from './img/git.png';
+import mysql from './img/mysql.png';
+import react from './img/react.png';
+import csharp from './img/csharp.png';
+import github from './img/github.svg';
+import linkedin from './img/linkedin.svg';
 
 function App() {
   return (
@@ -33,24 +39,38 @@ function App() {
           2
         </div>
 
+        <div class="div3">
+          <div className='div3-1'><img src={linkedin} alt="html" />LinkedIn</div>
+          <div className='div3-2'><img src={github} alt="html" />GitHub</div>
+          <div className='div3-3'>algo</div>
+        </div>
+
         <div class="div4">
           4
         </div>
+
         <div class="div7">
-          7
+          <div className='div7-1'>1</div>
+          <div className='div7-2'>2</div>
+          <div className='div7-3'>3</div>
+          <div className='div7-4'>4</div>
         </div>
 
         <div class="div8">
-          <h3>Technology</h3>
           <div class="image-row">
             <img src={html} alt="html" />
             <img src={sass} alt="sass" />
             <img src={js} alt="js" />
             <img src={css} alt="css" />
+            <img src={net} alt="net" />
+            <img src={csharp} alt="csharp" />
+            <img src={git} alt="git" />
+            <img src={mysql} alt="mysql" />
+            <img src={react} alt="react" />
           </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
